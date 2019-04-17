7fb82fbf915981d5bda7756413273e0865a6aae5
amazinzai <zomgitzaza@gmail.com>
2019-04-16 23:58:49 -0700

    Adding HoneyMart

---
+++ myscript.js
@@ -0,0 +1,17 @@
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+
+<!-- second pop up -->
+
+// When the user clicks on <div>, open the popup
+function myFunction() {
+var popup = document.getElementById("myPopup");
+popup.classList.toggle("show");
+}
