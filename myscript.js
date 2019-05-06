e777b86d0eae94ef6f1518767560fa52b6059d9d
amazinzai <zomgitzaza@gmail.com>
2019-05-06 11:19:16 -0700

    styling cart number button

--- myscript.js
+++ myscript.js
@@ -45,19 +45,7 @@ $('[data-toggle="popover-click"]').popover({
 })
 })
 
-// SHopping cart
-var itemCount = 0;
 
-$('.add').click(function (){
-  itemCount ++;
-  $('#itemCount').html(itemCount).css('display', 'block');
-});
-
-$('.clear').click(function() {
-  itemCount = 0;
-  $('#itemCount').html('').css('display', 'none');
-  $('#cartItems').html('');
-});
 
 // shopping test 2
 
